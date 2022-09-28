4ffeaef4908a48564764dda448cb93a5e20dabdc
muharrem gem <muharremgem28@gmail.com>
2022-09-28 16:04:51 +0300

    api

--- src/App.js
+++ src/App.js
@@ -18,7 +18,8 @@ const movie1 = {
 };
 
 const App = () => {
-  const [movies, setMovies] = useState();
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
@@ -38,10 +39,14 @@ const App = () => {
       <div className="search">
         <input
           placeholder="Search for movies"
-          value="Batman"
-          onChange={() => {}}
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
         />
-        <img src={SearchIcon} alt="search" onClick={() => {}} />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => searchMovies(searchTerm)}
+        />
       </div>
       {movies?.length > 0 ? (
         <div className="container">
